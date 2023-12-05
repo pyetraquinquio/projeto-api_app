f1ad6a290f246bdd099dc5f1cdfd670254e3421a
gabyycocielli <gabriellycocielli@gmail.com>
2023-12-05 14:33:28 -0300

    fg

--- app/pokemon/page.jsx
+++ app/pokemon/page.jsx
@@ -1,21 +1,23 @@
 "use client"
 
 import Cabecalho from "@/components/Cabecalho";
-import styles from "../pagina2/teste1.module.css";
+import styles from "../pokemon/pokemon.module.class";
 import Rodape from "@/components/Rodape";
 
 function Pokemon() {
     return (
         <div>
             <Cabecalho></Cabecalho>
-            <div className={styles.clteste}>
-                <div className={styles.lado1}>
+            <div>
+                <div className={styles.header}>
                     <h1>Pokedex</h1>
                     <br></br>
+                    <div className={styles.texto1}>
                     <h3>Anime Pokémon</h3>
                     <br></br>
                     <h4>Com o sucesso dos jogos Pokémon Red e Pokémon Green, no Japão, não demorou muito para que sua versão animada fosse lançada.
 A série de anime Pocket Monsters(1997), conta com mais de mil episódios divididos em 23 temporadas e 7 séries principais: Pokémon, Geração Avançada, Diamante e Pérola, Preto e Branco, XY, Sol e Lua, e Jornadas. Durante todos esses anos a história segue o garoto de 10 anos, Ash Ketchum, e seu parceiro Pikachu na sua jornada para se tornar um Mestre Pokémon.</h4>
+</div>
                 </div>
             </div>
             <div>
