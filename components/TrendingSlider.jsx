967503520cdb4ad0b0768917317f8a7ca10cabc3
Mariabarbo <eduarda.maria140806@gmail.com>
2023-12-11 15:59:44 -0300

    k.,mnjhgf

--- components/TrendingSlider.jsx
+++ components/TrendingSlider.jsx
@@ -8,36 +8,32 @@ const TrendingSlider = () => {
   const filteredItems = [
     {
         id: 1,
-        img: "/public/slide/poke1.jpg",
-        description: "Barroco",
-        Link: "/pagArtes/barroco"
+        img: "/slide/poke1.jpg",
+        
+        
       },
       {
         id: 2,
-        img: "public/slide/poke2.jpg",
-        description: "Rococó",
-        Link: "/pagArtes/rococo"
+        img: "/slide/poke2.jpg",
+        
   
       },
       {
         id: 3,
-        img: "public/slide/poke3.jpg",
-        description: "Renascimento",
-        Link: "/pagArtes/renascimento"
+        img: "/slide/poke3.jpg",
+        
   
       },
       {
         id: 4,
-        img: "public/slide/poke4.jpg",
-        description: "Impressionismo",
-        Link: "/pagArtes/impressionismo"
+        img: "/slide/poke4.jpg",
+       
   
       },
       {
         id: 5,
-        img: "public/slide/poke5.jpg",
-        description: "Romantismo",
-        Link: "/pagArtes/romantismo"
+        img: "/slide/poke5.jpg",
+       
   
       }
 
@@ -59,19 +55,17 @@ const TrendingSlider = () => {
         <div className="container">
           <div className="title-btns">
             <h3></h3>
-            <div className="btns">
-              <button title="scroll left" onClick={slideLeft}>
-                <AiOutlineArrowLeft />
-              </button>
-              <button title="scroll right" onClick={slideRight}>
-                <AiOutlineArrowRight />
-              </button>
-            </div>
           </div>
           <div className="row-container" id="slider">
             {filteredItems.map((item) => (
               <div key={item.id} className="row-item">
-              </div>
+                  <div className="item-header">
+                    <img src={item.img} alt="product" />
+                  </div>
+            </div>
+                 
+             
+
             ))}
           </div>
         </div>
