ade6c83413af8c41c0ce577783b8b6ccb75c5381
Mariabarbo <eduarda.maria140806@gmail.com>
2023-12-11 11:39:43 -0300

    ugfhgjkl

---
+++ src/app.tsx
@@ -0,0 +1,28 @@
+
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css' ;
+
+export default class Carrossel extends React.Component {
+    render() {
+        return (
+            <Swiper className='swiper-container'>
+                <SwiperSlide className='slide-item'>
+                <img src={require('../public/amarelopokemon.jpg')}/>
+                </SwiperSlide>
+
+                <SwiperSlide className='slide-item'>
+                <img src={require('../public/pokemon azulverde.jpg')}/>
+                </SwiperSlide>
+
+                <SwiperSlide className='slide-item'>
+                <img src={require('../public/pokemons laranazulverde.jpg')}/>
+                </SwiperSlide>
+
+            </Swiper>
+        );
+
+    }
+}
+
+
